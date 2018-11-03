743397369f515db959696fc0b82e49d9c4b1b17d
Rafal Dziegiel <rafstar@interia.eu>
2018-11-03 14:19:12 +0100

    Created API controller

---
+++ api/controllers/controller.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+Docs = mongoose.model('Docs');
+
+exports.list_all_docs = function(req, res) {
+  Docs.find({}, function(err, doc) {
+    if (err)
+      res.send(err);
+    res.json(doc);
+  });
+};
+
+exports.create_a_doc = function(req, res) {
+  var new_doc = new Docs(req.body);
+  new_doc.save(function(err, doc) {
+    if (err)
+      res.send(err);
+    res.json(doc);
+  });
+};
+
+exports.read_a_doc = function(req, res) {
+  Docs.findById(req.params.docId, function(err, doc) {
+    if (err)
+      res.send(err);
+    res.json(doc);
+  });
+};
+
+exports.update_a_doc = function(req, res) {
+  Docs.findOneAndUpdate({_id: req.params.docId}, req.body, {new: true}, function(err, doc) {
+    if (err)
+      res.send(err);
+    res.json(doc);
+  });
+};
+
+exports.delete_a_doc = function(req, res) {
+  Docs.remove({
+    _id: req.params.docId
+  }, function(err, doc) {
+    if (err) res.send(err);
+    res.json({ message: 'Document successfully deleted' });
+  });
+};
