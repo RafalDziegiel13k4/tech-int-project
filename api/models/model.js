9598e24ab0c690cf6e8d302c38638d212d7aaa85
Rafal Dziegiel <rafstar@interia.eu>
2018-11-09 11:06:21 +0100

    Improved API display date format

--- api/models/model.js
+++ api/models/model.js
@@ -7,8 +7,12 @@ var docsSchema = new Schema({
     required: 'Enter name'
   },
   Modification_date: {
-    type: Date,
-    default: Date.now
+    type: String,
+    default: function() {
+      var options = { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+      var currentTime = new Date();
+      return currentTime.toLocaleDateString("pl-PL", options)
+      }
   },
   status: {
       type: String,
