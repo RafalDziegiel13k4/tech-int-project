89636bab78f171f253afb250955b75cd60655b87
Rafal Dziegiel <rafstar@interia.eu>
2018-12-16 12:38:13 +0100

    Include Cross-Origin Resource Sharing

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+
 const port = process.env.PORT || 3000;
 const Task = require('./api/models/model');
 const routes = require('./api/routes/routes');
