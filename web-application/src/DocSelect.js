6338424eb2e3f16e60e8ba78409db5b3d194fe46
Rafal Dziegiel <rafstar@interia.eu>
2018-12-31 15:16:19 +0100

    Inform user about empty database

--- web-application/src/DocSelect.js
+++ web-application/src/DocSelect.js
@@ -60,20 +60,29 @@ class DocSelect extends Component {
         let docList = [];
         const { apiData } = this.state;
 
-        for (let i = 0; i < apiData.length; i++) {
-            let disableButton = false;
-
-            if (apiData[i].status === "used") {
-                disableButton = true;
-            }
-
-            docList.push(<div className="documentBox" key={i}>
-                <h3>{apiData[i].name}</h3>
-                <p>Status: {apiData[i].status}</p>
-                <a href="http://localhost:3002/editor"><button className="small green button">View</button></a><br/>
-                <a href="http://localhost:3002/editor"><button className="small blue button" disabled={disableButton}>Edit</button></a><br/>
-                <button className="small red button" disabled={disableButton} onClick={this.delDocsFromApi.bind(this, apiData[i]._id)}>Delete</button>
+        if(apiData.length === 0)
+        {
+            docList.push(<div className="documentBox" key="0">
+                <p>DATABASE IS EMPTY</p>
             </div>);
+        }
+        else
+        {
+            for (let i = 0; i < apiData.length; i++) {
+                let disableButton = false;
+
+                if (apiData[i].status === "used") {
+                    disableButton = true;
+                }
+
+                docList.push(<div className="documentBox" key={i}>
+                    <h3>{apiData[i].name}</h3>
+                    <p>Status: {apiData[i].status}</p>
+                    <a href="http://localhost:3002/editor"><button className="small green button">View</button></a><br/>
+                    <a href="http://localhost:3002/editor"><button className="small blue button" disabled={disableButton}>Edit</button></a><br/>
+                    <button className="small red button" disabled={disableButton} onClick={this.delDocsFromApi.bind(this, apiData[i]._id)}>Delete</button><br/>
+                </div>);
+            }
         }
         return docList;
     };
